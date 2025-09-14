ce966893d26f80e756ddf4cdf494e3603cfc7ad3
Zarcotech <zidan.harb2012@gmail.com>
2025-09-14 16:30:07 -0500

    FINALLY THE ROBOT IS MOVING CORRECTLY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.bylazar.field.Line;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.follower.Follower;
@@ -7,58 +8,46 @@ import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.pedropathing.geometry.Pose;
 
 import org.firstinspires.ftc.teamcode.pedropathing.Constants;
 
 @Autonomous(name = "Auto Testing Cyber Salam FTC")
 public class Auto extends OpMode {
 
-    private DcMotor leftFront;
-    private DcMotor leftRear;
-    private DcMotor rightFront;
-    private DcMotor rightRear;
-
     private Follower follower;
     private PathChain path;
 
     @Override
     public void init() {
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
 
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftRear.setDirection(DcMotor.Direction.REVERSE);
 
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Pose startPose = new Pose(0, 0, 0);
 
         // Pass the hardware map to the createFollower method
-        follower = Constants.createFollower(hardwareMap);
-        if (follower == null || follower.getDrivetrain() == null) {
-            telemetry.addLine("Follower or Drivetrain not initialized!");
-            telemetry.update();
-            return;
-        }
 
-        Pose start = new Pose(0, 0, 0);
-        Pose end = new Pose(0, 48, 0);
+
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+//        Pose start = new Pose(0, 0, 0);
+        Pose middle = new Pose(0, 10, 0);
+        Pose end = new Pose(0, 0, 0);
 
         path = follower.pathBuilder()
-                .addPath(new BezierLine(start, end))
-                .setLinearHeadingInterpolation(start.getHeading(), end.getHeading())
+                .addPath(new BezierLine(startPose, middle))
+                .setLinearHeadingInterpolation(startPose.getHeading(), middle.getHeading())
+                .addPath(new BezierLine(middle, end))
+                .setLinearHeadingInterpolation(middle.getHeading(), end.getHeading())
                 .build();
+    }
+
+    @Override
+    public void start() {
+        follower.update();
         follower.followPath(path);
-
-
     }
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
@@ -16,40 +16,36 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.cybersalam.hardware.MecanumDrive;
 
 public class Constants {
-    public Drivetrain drivetrain;
-
-    public Drivetrain getDrivetrain() {
-        return drivetrain;
-    }
-
     public static FollowerConstants followerConstants = new FollowerConstants();
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-5)
-            .strafePodX(0.5)
+
+            .forwardPodY(5)
+            .strafePodX(5)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpointComputer")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
-    public static Follower createFollower(HardwareMap hardwareMap) {
-        MecanumConstants drivetrain = new MecanumConstants()
-                .maxPower(1)
-                .rightFrontMotorName("rightFront")
-                .rightRearMotorName("rightRear")
-                .leftRearMotorName("leftRear")
-                .leftFrontMotorName("leftFront")
-                .leftFrontMotorDirection(DcMotor.Direction.REVERSE)
-                .leftRearMotorDirection(DcMotor.Direction.REVERSE)
-                .rightFrontMotorDirection(DcMotor.Direction.FORWARD)
-                .rightRearMotorDirection(DcMotor.Direction.FORWARD);
-        return new FollowerBuilder(followerConstants, hardwareMap)
+    public static MecanumConstants drivetrain = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftRearMotorName("leftRear")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotor.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotor.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotor.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotor.Direction.REVERSE);
 
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
                 .pinpointLocalizer(localizerConstants)
+                .mecanumDrivetrain(drivetrain)
                 .build();
     }
 
