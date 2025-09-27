b402128da47eb3bafdde4ee6297207bc6c6d4e94
Zarcotech <zidan.harb2012@gmail.com>
2025-09-27 12:14:39 -0500

    Add zero power behavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.pedropathing;
 
 import com.pedropathing.Drivetrain;
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,16 +16,69 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
-    public static PathConstraints pathConstraints = new PathConstraints(0.5, 50, 20, 20);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .forwardZeroPowerAcceleration(-25.9346931313679598)
+            .lateralZeroPowerAcceleration(-67.342491844080064)
+            .translationalPIDFCoefficients(new PIDFCoefficients(
+                    0,
+                    0,
+                    0,
+                    0.015
+            ))
+            .translationalPIDFSwitch(4)
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(
+                    0.4,
+                    0,
+                    0.005,
+                    0.0006
+            ))
+            .headingPIDFCoefficients(new PIDFCoefficients(
+                    0.9,
+                    0,
+                    0,
+                    0.01
+            ))
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(
+                    2.7,
+                    0,
+                    0.1,
+                    0.0005
+            ))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(
+                    0.9,
+                    0,
+                    0.00035,
+                    0.6,
+                    0.015
+            ))
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(
+                    0.08,
+                    0,
+                    0.000005,
+                    0.6,
+                    0.01
+            ))
+            .drivePIDFSwitch(15)
+            .centripetalScaling(0.0005);
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            0.1,
+            0.1,
+            0.009,
+            50,
+            1.25,
+            10,
+            1
+    );
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(4)
             .strafePodX(-7.0)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpointComputer")
+            .customEncoderResolution(505.09)
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
     public static MecanumConstants drivetrain = new MecanumConstants()
             .maxPower(1)
